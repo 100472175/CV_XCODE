542be1796850ba9950d1a8e6135d3ad8fdd1e9be
100472175 <100472175@alumnos.uc3m.es>
2023-02-25 09:49:12 +0100

    Initial Commit

---
+++ CV/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  CV
+//
+//  Created by Eduardo Alarcon on 25/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
